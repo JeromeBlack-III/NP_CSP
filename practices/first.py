081c6a16e3d1444ced7aa52f3ee0ca001ddc7c46
JeromeBlack-III <nicholas.price@ucas-edu.net>
2026-01-22 11:51:20 -0700

    fixedbugs

---
+++ practices/first.py
@@ -0,0 +1,6 @@
+food=input ("Tell me a food: ") 
+
+
+print(food, "isn't as good as bacon")
+
+
---
+++ practices/variables_practices.py
@@ -0,0 +1,24 @@
+# NP, Variables Practice
+
+name = "Jerome"
+
+number = 7
+
+number2 = 420
+
+breakfast= "Bacon"
+
+color= "Green"
+
+school= "UCAS"
+
+year= 2026
+
+eye= "browm"
+
+age= 14
+
+subject= "biology"
+
+print(name, number, number2, breakfast, color, school, year, eye, age, subject)
+
