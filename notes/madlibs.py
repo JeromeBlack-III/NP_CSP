f531f4923e5acc83012320cba9281a6cbb0ebda5
JeromeBlack-III <nicholas.price@ucas-edu.net>
2026-01-28 11:39:49 -0700

    madlibs.py

---
+++ notes/madlibs.py
@@ -0,0 +1,13 @@
+Noun1= input("Give a noun(person): ")
+
+Noun2= input("Give another noun(person): ")
+
+Noun3=input("Give a example of an Object: ")
+
+Noun4=input("Give a example of a Person,Place, or Thing: ")
+
+Noun5=input("Give a example of a Person,Place, or Thing: ")
+
+Noun6=input("Give a example of a plural Object: ")
+
+print(Noun1+ " went to the beach with " +Noun2+ "." "First they swam in their " +Noun3+ "." "Then, they saw " +Noun4+ " and "  +Noun5+ " in the ocean!" " Lasly, they built a sandcastle out of " +Noun6+ "")
