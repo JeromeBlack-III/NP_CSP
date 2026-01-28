a16129cb42196ee89dfc06fe4c13674dadb5e8b9
JeromeBlack-III <nicholas.price@ucas-edu.net>
2026-01-28 11:45:22 -0700

    libs.py

--- practices/first.py
+++ practices/first.py
@@ -1,6 +1,13 @@
-food=input ("Tell me a food: ") 
+Noun1= input("Give a noun(person): ")
 
+Noun2= input("Give another noun(person): ")
 
-print(food, "isn't as good as bacon")
+Noun3=input("Give a example of an vehicle: ")
 
+Noun4=input("Give a example of a Person,Place, or Thing: ")
 
+Noun5=input("Give a example of a Person,Place, or Thing: ")
+
+Noun6=input("Give a example of a plural Object: ")
+
+print(Noun1+ " went to the beach with " +Noun2+ "." "First they swam in their " +Noun3+ "." "Then, they saw " +Noun4+ " and "  +Noun5+ " in the ocean!" " Lasly, they built a sandcastle out of " +Noun6+ "")
