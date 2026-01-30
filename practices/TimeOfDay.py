ee2f0f43c11a52d84f6558c676b8538fffc77878
JeromeBlack-III <nicholas.price@ucas-edu.net>
2026-01-30 11:35:41 -0700

    clock

---
+++ practices/TimeOfDay.py
@@ -0,0 +1,9 @@
+number=int(input("In millitary time, what time is it: "))
+
+if number >1159:
+    print("Good Afternoon")
+elif number >559:
+    print(" Good Morning")
+else:
+    print("good evening")
+
