8d36a18aa0ce7cc6e0fc3e6f7dd89e539e0dce1b
JeromeBlack-III <nicholas.price@ucas-edu.net>
2026-02-12 11:54:49 -0700

    team game

--- team_game.py
+++ team_game.py
@@ -1,3 +1,4 @@
+import random
 name = input("Enter a Name: ")
 
 print( "Hi " +name+ "!  Welcome to Tic Tac Toe")
@@ -18,16 +19,71 @@ def show_board(board_spot):
 
 board_spot=[0,1,2,3,4,5,6,7,8]  
 
+def win (board_spot):
+    if board_spot[0]==board_spot[1]==board_spot[2]:
+        print("You Win!")
+        return True
+    elif board_spot[3]==board_spot[4]==board_spot[5]:
+        print("You Win!")
+        return True
+    elif board_spot[6]==board_spot[7]==board_spot[8]:
+        print("You Win!")
+        return True
+   
+    elif board_spot[0]==board_spot[4]==board_spot[8]:
+        print("You Win!")
+        return True
+    elif board_spot[6]==board_spot[4]==board_spot[2]:
+        print("You Win!")
+        return True
+    elif board_spot[6]==board_spot[7]==board_spot[8]:
+        print("You Win!")
+        return True
+   
+    elif board_spot[0]==board_spot[3]==board_spot[6]:
+        print("You Win!")
+        return True
+    elif board_spot[1]==board_spot[4]==board_spot[7]:
+        print("You Win!")
+        return True
+    elif board_spot[2]==board_spot[5]==board_spot[8]:
+        print("You Win!")
+        return True
+
+
+
+
 
 while True:
+    
     complete_board= show_board(board_spot)
     print(complete_board)
     box_select= int(input("Select a box (0-8): "))
     board_spot[box_select] = "X"
 
     complete_board= show_board(board_spot)
+    
+    
+    #print(random.choice(board_spot))
 
+    if win(board_spot):
+        break
+   
+   
+    #break
+  
+  
+  
+  
+  
+   
+    
+    
+    
+    
 
+    
+  
 
 
 
