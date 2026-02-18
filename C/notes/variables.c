0b9c7fd505ec5953b53f54256abf8b5774305f2c
JeromeBlack-III <nicholas.price@ucas-edu.net>
2026-02-18 11:49:07 -0700

    c

---
+++ C/notes/variables.c
@@ -0,0 +1,12 @@
+// This is a comment in C
+#include <stdio.h> // allows for creation of inputs and outputs 
+
+int main(){
+    int number= 12; 
+    float pi = 3.14;
+   
+    printf("%d\n", number); // \n tells the computer to move the lo the next line 
+    printf("%f\n", pi);
+    return 0; //always the last line of main 
+}
+
