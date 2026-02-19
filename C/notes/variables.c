b331ea83db99a4c52af44801baa06c166ba2de81
JeromeBlack-III <nicholas.price@ucas-edu.net>
2026-02-19 11:52:08 -0700

    variblesc

--- C/notes/variables.c
+++ C/notes/variables.c
@@ -4,9 +4,19 @@
 int main(){
     int number= 12; 
     float pi = 3.14;
+    char name[ ] = "Xavier";
+    char person[50];
+    int age; 
+
+    printf("Tell me your name\n");
+    scanf("%d", &person);
+   
+    printf("Tell me how old you are\n");
+    scanf("%d", &age);
    
     printf("%d\n", number); // \n tells the computer to move the lo the next line 
     printf("%f\n", pi);
+    printf("%s is %d years old\n", name, age); 
     return 0; //always the last line of main 
 }
 
---
+++ C/notes/variablespratice.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+
+    char name[ ] = "Jerome";
+    int number= 7; 
+    float gpa = 3.14;
+    char breakfast[]= "Milkshake";
+    char color[ ]= "Green" ; 
+    char  school[ ]= "UCAS" ; 
+    int year= 2026 ; 
+    char eye[ ]= "Brown" ; 
+    int age= 14 ; 
+    char subject[ ]= "Biology" ; 
+
+    printf("%s\n", name);
+    printf("%d\n", number);
+    printf("%f\n", gpa);
+    printf("%s\n", breakfast);
+    printf("%s\n", color);
+    printf("%s\n", school);
+    printf("%d\n", year);
+    printf("%s\n", eye);
+    printf("%d\n", age);
+    printf("%s\n", subject);
+   
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
