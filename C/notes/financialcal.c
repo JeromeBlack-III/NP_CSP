0eb418be784b1a49fba2d7d8bca9ce845d587e6f
JeromeBlack-III <nicholas.price@ucas-edu.net>
2026-02-24 16:13:00 -0700

    calcl.cye

--- C/notes/financialcal.c
+++ C/notes/financialcal.c
@@ -28,20 +28,25 @@ printf("Tell me your transportation cost\n");
 
 
 scanf("%d", &trans);
-float inr = (float)rent/income;
-printf("%d is your rent, and that is %.2f precent of your income\n", rent, inr);
+float inr = (float)rent/income*100;
+printf("$%d is your rent, and that is %.2f  precent of your income\n", rent, inr);
 
-float inu = (float)utilities/income;
-printf("%d is your groceries, and that is %.2f*100 precent of your income\n", utilities, inu);
+float inu = (float)utilities/income*100;
+printf("$%d is your utility expenses, and that is %.2f precent of your income\n", utilities, inu);
 
-float ing = (float)groceries/income;
-printf("%d is your groceries, and that is %.2f*100 precent of your income\n", groceries, ing);
+float ing = (float)groceries/income*100;
+printf("$%d is your groceries expenses, and that is %.2f precent of your income\n", groceries, ing);
+
+float intr = (float)trans/income*100;
+printf("$%d is your transportation expenses, and that is %.2f precent of your income\n", trans, intr);
+
+float save = (float)income*.1;
+printf("$%.2f is your svaings,  %.2f precent of your income\n", save, 10.0);
 
 
+float end = (float)income-rent-utilities-groceries-trans-save;
 
-
-
-
+printf("$%.2f is how much is left for spending", end);
 
 
 
