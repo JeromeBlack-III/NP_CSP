17a1175ab8e9b04bf7078622bf6ab83c9a41ff95
JeromeBlack-III <nicholas.price@ucas-edu.net>
2026-03-06 11:50:28 -0700

    calc.ccc

--- C/notes/financialcal.c
+++ C/notes/financialcal.c
@@ -2,53 +2,56 @@
 #include <math.h>
 
 
-float incomeprecent(float cost, float income){
-    return cost/income *100;
+float incomeprecent(int cost, int income){
+    return (float)cost/income *100;
 }
+ 
+int tellme(char* amount){
+    int costof;
+    printf("Tell me your %s amount: ", amount);
+    scanf("%d", &costof );
+return costof;
 
 
 
 
+
+
+}
 
 int main(){
 
-int income ;
+int income;
 int rent;
 int utilities;
 int groceries;
 int trans;
 
-printf("Tell me your income\n");
-scanf("%d", &income);
+income = tellme("income");
 
+rent = tellme("rent");
 
-printf("Tell me your rent cost\n");
-scanf("%d", &rent);
+utilities = tellme("utilities");
 
-printf("Tell me your utilities cost\n");
-scanf("%d", &utilities);
+groceries = tellme("groceries");
 
-printf("Tell me your groceries cost\n");
-scanf("%d", &groceries);
-
-printf("Tell me your transportation cost\n");
-scanf("%d", &trans);
+trans = tellme("transportation");
 
 
 float inr = incomeprecent(rent,income);
 printf("$%d is your rent, and that is %.2f  precent of your income\n", rent, inr);
 
-float inu = incomeprecent(rent,utilities);
+float inu = incomeprecent(utilities,income);
 printf("$%d is your utility expenses, and that is %.2f precent of your income\n", utilities, inu);
 
-float ing = incomeprecent(rent,groceries);
+float ing = incomeprecent(groceries,income);
 printf("$%d is your groceries expenses, and that is %.2f precent of your income\n", groceries, ing);
 
-float intr = incomeprecent(rent,trans);
+float intr = incomeprecent(trans,income);
 printf("$%d is your transportation expenses, and that is %.2f precent of your income\n", trans, intr);
 
 float save = (float)income*.1;
-printf("$%.2f is your svaings,  %.2f precent of your income\n", save, 10.0);
+printf("$%.2f is your savings,  %.2f precent of your income\n", save, 10.0);
 
 
 float end = (float)income-rent-utilities-groceries-trans-save;
